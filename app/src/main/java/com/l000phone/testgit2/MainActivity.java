05e079c28a97e2d08c28803fd879daebb940cffb
jansonxu <jansonxu@sina.cn>
2016-07-01 00:15:46 +0800

    修改MainActivity.java

--- app/src/main/java/com/l000phone/testgit2/MainActivity.java
+++ app/src/main/java/com/l000phone/testgit2/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //世界你好！我是徐文波哦！
     }
 }
